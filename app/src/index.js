c27b2d949697367f260bad774b9fcc0f6f5eaf2b
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-04-19 11:51:37 +0530

    feat: add Prometheus metrics collection and expose /metrics endpoint

--- app/src/index.js
+++ app/src/index.js
@@ -17,3 +17,39 @@ app.get('/go', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+// const express = require('express');
+const client = require('prom-client');
+
+// Enable collection of default metrics (CPU, memory, event loop lag etc.)
+client.collectDefaultMetrics();
+
+// Custom metrics
+const httpRequestDurationMicroseconds = new client.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'status_code'],
+  buckets: [50, 100, 200, 300, 400, 500, 1000] // in ms
+});
+
+// Middleware to measure duration
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.route?.path || req.path, status_code: res.statusCode });
+  });
+  next();
+});
+
+// Your routes
+app.get('/', (req, res) => res.send('Hello World'));
+
+// /metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
